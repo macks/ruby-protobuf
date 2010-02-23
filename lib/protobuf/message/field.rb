2d1824b309bf79ca14360bdb275764781ade7b0a
MATSUYAMA Kengo <macksx@gmail.com>
2010-02-24 03:05:30 +0900

    Suppress warnings.

--- lib/protobuf/message/field.rb
+++ lib/protobuf/message/field.rb
@@ -640,7 +640,7 @@ module Protobuf
             when field.type
               @values[field.name] = val
             else
-              raise TypeError, "Expected value of type '#{field.type}', but got '#{val.type}'"
+              raise TypeError, "Expected value of type '#{field.type}', but got '#{val.class}'"
             end
           end
         end
